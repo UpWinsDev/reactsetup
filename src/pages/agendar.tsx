59a4959e628bda09e10d22b75e3ea40212418674
Eddiesantle <62967219+Eddiesantle@users.noreply.github.com>
2022-05-09 09:31:57 -0300

    up

---
+++ src/pages/agendar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SideBar from '../../src/components/SideBar'
+
+// import { Container } from './styles';
+
+const Agendar: React.FC = () => {
+    return <SideBar children={<h1>Agende sua Consulta</h1>} />
+}
+
+export default Agendar
---
+++ src/pages/pacientes.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SideBar from '../../src/components/SideBar'
+
+// import { Container } from './styles';
+
+const Pacientes: React.FC = () => {
+    return <SideBar children={<h1>Lista de Pacientes</h1>} />
+}
+
+export default Pacientes
