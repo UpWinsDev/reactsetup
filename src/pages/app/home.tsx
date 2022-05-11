d8f901da7ce785e322ffabc6c980079f7bbeb76c
Eddiesantle <62967219+Eddiesantle@users.noreply.github.com>
2022-05-11 19:32:29 -0300

    up

--- src/pages/app/home.tsx
+++ src/pages/app/home.tsx
@@ -8,9 +8,9 @@ export default function Home() {
     return (
         <SideBar
             children={
-                <div className="h-screen gb-grey-50 flex justify-center">
+                <div className="h-screen gb-grey-50 flex justify-center grid-rows-2">
    
-                    <div className="w-full max-w-screen-lg grid grid-cols-1 border md:grid-cols-4 gap-x-5 md:max-w-2xl">
+                    <div className="w-full max-w-screen-lg grid grid-cols-1 border md:grid-cols-6 gap-x-5 md:max-w-2xl">
                         <div className="...">
                             <div className="bg-white rounded-lg shadow-xl">
                                 <div className="h-24"></div>
@@ -32,30 +32,7 @@ export default function Home() {
                                 <div className="h-24"></div>
                             </div>
                         </div> 
-               
-                        
                     </div>
-                    <div className="w-full max-w-screen-lg grid grid-cols-1 border md:grid-cols-4 gap-x-5 md:max-w-2xl">
-                        <div className="...">
-                            <div className="bg-white rounded-lg shadow-xl">
-                                <div className="h-24"></div>
-                                
-                            </div>
-                        </div>
-                        <div className="...">
-                            <div className="bg-white rounded-lg shadow-xl">
-                                <div className="h-24"></div>
-                            </div>
-                        </div>   
-                        <div className="...">
-                            <div className="bg-white rounded-lg shadow-xl">
-                                <div className="h-24"></div>
-                            </div>
-                        </div>  
-               
-                        
-                    </div>
-                   
                 </div>
             }
         />
