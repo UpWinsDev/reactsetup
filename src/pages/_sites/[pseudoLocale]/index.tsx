cc731ccbde042ab3c8fb8a7a35662a48dc554395
Eddie Santos <62967219+Eddiesantle@users.noreply.github.com>
2022-05-18 09:57:55 -0300

    navbar

--- src/pages/_sites/[pseudoLocale]/index.tsx
+++ src/pages/_sites/[pseudoLocale]/index.tsx
@@ -56,7 +56,7 @@ const Home: React.FC = ({ pseudoLocale }: AppProps) => {
             <main className="main">
 
 
-                <div className="min-h-full bg-white rounded-lg border border-gray-200 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                <div className="min-h-full bg-white rounded-lg border border-gray-200 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 shadow-md">
                     <div className="max-w-md w-full space-y-8">
                     <div>
 
